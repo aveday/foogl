6b60c3442fcb3556fba47d1f2368edf107573f87
David Williams <david@aveday.net>
2016-04-27 23:15:30 +1000

    Add colors.h, required for previous commit

---
+++ src/colors.h
@@ -0,0 +1,9 @@
+#include <glm/glm.hpp>
+
+static const glm::vec4 red  (1.0f, 0.0f, 0.0f, 1.0f);
+static const glm::vec4 green(0.0f, 1.0f, 0.0f, 1.0f);
+static const glm::vec4 blue (0.0f, 0.0f, 1.0f, 1.0f);
+
+static const glm::vec4 darkRed  (0.5f, 0.0f, 0.0f, 1.0f);
+static const glm::vec4 darkGreen(0.0f, 0.5f, 0.0f, 1.0f);
+static const glm::vec4 darkBlue (0.0f, 0.0f, 0.5f, 1.0f);
