bfe060744d16db0c2e14a2841e8bf622b04a929e
David Williams <david@aveday.net>
2016-04-27 23:14:22 +1000

    Change camera constructor to take pos and dir

--- src/Camera.h
+++ src/Camera.h
@@ -7,11 +7,12 @@ class Camera
 {
     public:
         glm::mat4 projectionMatrix, viewMatrix;
-
-        Camera(float fov, float aspect, float near, float far);
+        Camera(glm::vec3 position, glm::vec3 rotation);
+        void configure(float fov, float aspect, float near, float far);
         void updateProjection(float aspect);
 
     private:
+        glm::vec3 position, direction;
         float fov, aspect, near, far;
 };
 
