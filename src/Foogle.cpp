a806c98e87ae5b344796fed38d5c78e404c45daf
David Williams <david@aveday.net>
2016-04-28 19:03:30 +1000

    Make Camera a subclass of Entity

--- src/Foogle.cpp
+++ src/Foogle.cpp
@@ -21,28 +21,37 @@ int main() {
     // create the shader program
     GLuint shader = loadProgram("glsl/flat.vs", "glsl/flat.fs");
 
-    // setup camera
-    Camera camera(vec3(0, 5, -10), vec3(0, -1, 2));
-    camera.configure(fov, aspect, near, far);
-
     // setup lighting
-    Light light(shader, vec3(0,5,10), white);
+    Light light(shader, vec3(0,7,-7), white);
 
     // setup clock
     Clock clock;
 
-    /*       SHADER  SIZE                  POSITION                 COLOR */
-    Box box (shader, vec3(5.0f,0.2f,5.0f), vec3(),                  darkRed);
-    Box wall(shader, vec3(0.2f,5.0f,5.0f), vec3(2.6f, 2.6f, 0),     darkBlue);
+    Box player(shader, vec3(1,1,1), vec3(0,3,0), green);
+    player.rotate(0, 0, 0);
 
+    /*       SHADER  SIZE                  POSITION          COLOR */
+    Box box (shader, vec3(5.0f,0.2f,5.0f), vec3(0,-2.6f, 0), darkRed);
+    Box wall(shader, vec3(0.2f,5.0f,5.0f), vec3(2.4f, 0, 0), darkBlue);
+    Box back(shader, vec3(5.0f,5.0f,0.2f), vec3(0, 0, 2.6f), darkGreen);
+
+    // setup camera
+    Camera camera(vec3(0, 0, -10));
+    camera.configure(fov, aspect, near, far);
+
+    player.add_child(&camera);
+
+    entities.push_front( &player);
     entities.push_front( &box );
     entities.push_front( &wall);
+    entities.push_front( &back);
 
     /* MAIN EVENT LOOP*/
     while(window.running())
     {
         float dt = clock.tick();
-        window.control(camera);
+        //player.rotate(0,0,100*dt);
+        window.control(player);
         window.render(camera, entities);
     }
     return 0;
