90629d256618404b04d7ced2baf2f8778d269354
David Williams <david@aveday.net>
2016-04-28 11:26:50 +1000

    Add white and Color typedef to colors.h

--- src/colors.h
+++ src/colors.h
@@ -1,4 +1,10 @@
+#pragma once
+
 #include <glm/glm.hpp>
+
+typedef glm::vec4 Color;
+
+static const glm::vec4 white (1.0f, 1.0f, 1.0f, 1.0f);
 
 static const glm::vec4 red  (1.0f, 0.0f, 0.0f, 1.0f);
 static const glm::vec4 green(0.0f, 1.0f, 0.0f, 1.0f);
