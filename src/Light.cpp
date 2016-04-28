b5d7b1a70be11b24e7359d8ce49eebeff9b2dbd9
David Williams <david@aveday.net>
2016-04-28 11:36:04 +1000

    Add Light.move() function

--- src/Light.cpp
+++ src/Light.cpp
@@ -3,12 +3,23 @@
 Light::Light(GLuint program, glm::vec3 position, Color color) :
     shaderProgram(program), position(position), color(color)
 {
-    glUseProgram(shaderProgram);
-    glUniform3f(
-            glGetUniformLocation(shaderProgram, "lightPosition"),
-            position.x, position.y, position.z);
+    updateShader();
+}
 
-    glUniform3f(
-            glGetUniformLocation(shaderProgram, "lightColor"),
-            color.x, color.y, color.z);
+void Light::move(glm::vec3 diff)
+{
+    position += diff;
+    updateShader();
+}
+
+void Light::updateShader()
+{
+    glUseProgram(shaderProgram);
+        glUniform3f(
+                glGetUniformLocation(shaderProgram, "lightPosition"),
+                position.x, position.y, position.z);
+
+        glUniform3f(
+                glGetUniformLocation(shaderProgram, "lightColor"),
+                color.x, color.y, color.z);
 }
--- src/Light.h
+++ src/Light.h
@@ -10,8 +10,10 @@ class Light
 {
     public:
         Light(GLuint program, glm::vec3 position, Color color = white);
+        void move(glm::vec3 diff);
 
     private:
+        void updateShader();
         GLuint shaderProgram;
         glm::vec3 position;
         glm::vec3 color;
