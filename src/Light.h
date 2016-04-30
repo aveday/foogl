1d2583cac39091f149c6b5aedc19910ecfb80d73
David Williams <david@aveday.net>
2016-04-30 18:00:47 +1000

    refactor update implementation

--- src/Light.h
+++ src/Light.h
@@ -10,8 +10,8 @@ class Light : public Entity
 {
     public:
         Light(GLuint shader, vec3 position, Color color = white);
-
         void updateShader(GLuint shader);
+
     private:
         vec3 color;
 };
