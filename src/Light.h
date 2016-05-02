631fabdb9cea594919866b73a5f9f60fd6fc5e3b
David Williams <david@aveday.net>
2016-05-03 00:55:46 +1000

    Implement trial UBO in Light constructor

--- src/Light.h
+++ src/Light.h
@@ -9,10 +9,15 @@
 class Light : public Entity
 {
     public:
-        Light(Mesh &mesh, vec3 position, Color color = white);
+        Light(GLuint shader, Mesh &mesh, vec3 position, Color color = white);
         void updateShader(GLuint shader);
 
     private:
+        float bufferdata[9];
+
+        GLuint ubo;
         vec3 color;
+        float attenuation = 0.2f;
+        float ambientCoefficient = 0.005f;
 };
 
