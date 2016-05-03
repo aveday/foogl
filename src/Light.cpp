f3dc2d7c02bdc5159a531a56e48e5d159966d871
David Williams <david@aveday.net>
2016-05-03 13:53:46 +1000

    Refactor UBO code in Light constructor & updater

--- src/Light.cpp
+++ src/Light.cpp
@@ -6,65 +6,39 @@
 Light::Light(GLuint shader, Mesh &mesh, vec3 position, Color color) :
     Entity(mesh, position, vec3(.01)), color(color)
 {
-    // get the uniform block index
-    GLuint index = glGetUniformBlockIndex(shader, "Light");
+    // get the index of the uniform block
+    GLuint blockIndex = glGetUniformBlockIndex(shader, "Light");
 
-    // find the size required
-    GLint blockSize;
-    glGetActiveUniformBlockiv(shader, index, GL_UNIFORM_BLOCK_DATA_SIZE, &blockSize);
- 
-    // create the buffer
-    GLubyte* blockBuffer = (GLubyte*)malloc(blockSize);
-
-    // query for the offsets of each block variable
+    // find the offsets of each block variable
     const GLchar *names[] = {
-        "lightposition",
-        "lightcolor",
-        "lightattenuation",
-        "lightambientCoefficient"
+        "Light.position",
+        "Light.color",
+        "Light.attenuation",
+        "Light.ambientCoefficient"
     };
-    
     GLuint indices[4];
     glGetUniformIndices(shader, 4, names, indices);
-
-    GLint offset[4];
     glGetActiveUniformsiv(shader, 4, indices, GL_UNIFORM_OFFSET, offset);
 
-    // write the data to the buffer
-    memcpy(blockBuffer + offset[0], glm::value_ptr(position), 3 * sizeof(GLfloat));
-    memcpy(blockBuffer + offset[1], glm::value_ptr(color), 3 * sizeof(GLfloat));
-    memcpy(blockBuffer + offset[2], &attenuation, sizeof(GLfloat));
-    memcpy(blockBuffer + offset[3], &ambientCoefficient, sizeof(GLfloat));
+    // find the size required for the buffer object
+    GLint blockSize;
+    glGetActiveUniformBlockiv(shader, blockIndex, GL_UNIFORM_BLOCK_DATA_SIZE, &blockSize);
 
-    // Create the OpenGL buffer object and copy the data into it.
+    // Create the OpenGL buffer object
     glGenBuffers(1, &ubo);
     glBindBuffer(GL_UNIFORM_BUFFER, ubo);
-    glBufferData(GL_UNIFORM_BUFFER, blockSize, blockBuffer, GL_DYNAMIC_DRAW );
+    glBufferData(GL_UNIFORM_BUFFER, blockSize, nullptr, GL_DYNAMIC_DRAW );
 
-    // bind the buffer
-    glBindBufferBase(GL_UNIFORM_BUFFER, index, ubo);
+    // bind the buffer to the uniform block
+    glBindBufferBase(GL_UNIFORM_BUFFER, blockIndex, ubo);
 }
 
 void Light::updateShader(GLuint shader)
 {
-    glUseProgram(shader);
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo);
 
-
-    /*
-    glUniform3f(
-            glGetUniformLocation(shader, "light.position"),
-            position.x, position.y, position.z);
-
-    glUniform3f(
-            glGetUniformLocation(shader, "light.color"),
-            color.x, color.y, color.z);
-
-    glUniform1f(
-            glGetUniformLocation(shader, "light.attenuation"),
-            attenuation);
-
-    glUniform1f(
-            glGetUniformLocation(shader, "light.ambientCoefficient"),
-            ambientCoefficient);
-    */
+    glBufferSubData(GL_UNIFORM_BUFFER, offset[0], 3*sizeof(GLfloat), glm::value_ptr(position));
+    glBufferSubData(GL_UNIFORM_BUFFER, offset[1], 3*sizeof(GLfloat), glm::value_ptr(color));
+    glBufferSubData(GL_UNIFORM_BUFFER, offset[2], 1*sizeof(GLfloat), &attenuation);
+    glBufferSubData(GL_UNIFORM_BUFFER, offset[3], 1*sizeof(GLfloat), &ambientCoefficient);
 }
