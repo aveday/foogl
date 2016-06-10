2f0aee8787d1f80782495eb06d0b59e6af5e054e
David Williams <david@aveday.net>
2016-06-10 18:19:12 +1000

    Integrate CameraC into RenderSystem, remove Entity

--- src/RenderSystem.cpp
+++ src/RenderSystem.cpp
@@ -1,19 +1,26 @@
 #include <glm/gtc/type_ptr.hpp>
 #include "EntityManager.h"
 #include "RenderSystem.h"
-#include "Component.h"
 
-void RenderSystem::run()
+void RenderSystem::run(CameraC &camera, GLuint shader)
 {
+    glUseProgram(shader);
+    // update the camera position uniform
+    vec3 position(camera.transform[3]);//XXX not sure about this
+    glUniform3fv(UNIFORM_CAMERA_POSITION, 1, glm::value_ptr(position));
+
+    // calculate and set the camera matrix
+    mat4 camera_matrix = camera.projection * glm::inverse(camera.transform);
+    glUniformMatrix4fv(UNIFORM_CAMERA_MATRIX, 1, 0, glm::value_ptr(camera_matrix));
+
     for(int e = 0; e < EM::end(); e++) {
-        if ( !EM::has_components<ModelC>(e) )
+        if (!EM::has_components<ModelC>(e))
             continue;
 
         ModelC &model = EM::get_component<ModelC>(e);
 
-
         if (model.mesh->vertices_n == 0)
-            return;
+            continue;
 
         // update uniform shader inputs
         glUniformMatrix4fv(UNIFORM_MODEL, 1, 0, glm::value_ptr(model.transform));
