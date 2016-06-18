af3ed19e06cda16070435700deb2ea67733b38d3
David Williams <david@aveday.net>
2016-06-19 04:02:15 +1000

    Add texture mapping

--- glsl/specular.vs
+++ glsl/specular.vs
@@ -4,16 +4,19 @@
 layout(location = 0) uniform mat4 camera;
 layout(location = 1) uniform mat4 model;
 
-layout (location = 0) in vec3 Position;
-layout (location = 1) in vec3 Normal;
+layout(location = 0) in vec3 Position;
+layout(location = 1) in vec3 Normal;
+layout(location = 2) in vec2 TexCoord;
 
 out vec3 fragPosition;
 out vec3 fragNormal;
+out vec2 fragTexCoord;
 
 void main()
 {
     fragPosition = vec3(model * vec4(Position, 1));
     fragNormal = normalize(transpose(inverse(mat3(model))) * Normal);
+    fragTexCoord = TexCoord;
 
     gl_Position = camera * vec4(fragPosition, 1);
 }
--- src/AssetLoader.h
+++ src/AssetLoader.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <GL/glew.h>
 #include <vector>
 #include <string>
@@ -7,22 +6,24 @@
 #include "glm.h"
 #include "assets.h"
 
+#define ASSET_DIR "assets/"
 
 typedef class AssetLoader {
 public:
     template <typename A> static inline int add_asset(A asset);
     static GLuint LoadProgram(const char* vs, const char* fs);
     static Mesh LoadMesh(MeshDef &def);
-    static Texture LoadTexture(std::string filename);
+    static GLuint LoadTexture(std::string filename);
 
 private:
     static GLuint LoadShader(GLenum type, const char *filename);
 
     static std::unordered_map<std::string, GLuint> program_cache;
-    static std::unordered_map<std::string, Texture> texture_cache;
+    static std::unordered_map<std::string, GLuint> texture_cache;
     static std::unordered_map<MeshDef*, Mesh> mesh_cache;
 
     static const GLuint INPUT_POSITION = 0;
     static const GLuint INPUT_NORMAL = 1;
+    static const GLuint INPUT_TEXCOORD = 2;
 } AL;
 
