fe231157bfc4a1c66d3a66d4d91d2145ff18198f
David Williams <david@aveday.net>
2016-06-22 16:58:20 +1000

    Remove MeshFlags

--- src/MeshGen.cpp
+++ src/MeshGen.cpp
@@ -11,36 +11,29 @@
 MeshDef MeshGen::PdpMesh( const int vertices = 1000,
                           const float size = 3.0f,
                           const float amplitude = 0.2f,
-                          const float period = 4,
-                          const MeshFlags flags )
+                          const float period = 4)
 {
     std::vector<vec3> positions, normals;
-    NormalType ntype = flags & FLAT ? SURFACE_NORMALS : POSITION_NORMALS;
 
     float d = 0.001f;
-    for (auto p : GeneratePoissonPoints(vertices, 30, flags & CIRCULAR)) {
+    for (auto p : GeneratePoissonPoints(vertices, 30)) {
 
         float x = size * (p.x - .5f);
         float z = size * (p.y - .5f);
         float y = stb_perlin_noise3(x*period, 0, z*period) * amplitude;
         positions.push_back({ x, y, z});
 
-        if (ntype == POSITION_NORMALS) {
-            float yx = stb_perlin_noise3(x*period,0,(z+d)*period) * amplitude;
-            float yz = stb_perlin_noise3((x+d)*period,0,z*period) * amplitude;
-            normals.push_back( get_normal( {x,y,z}, {x+d,yx,z}, {x,yz,z+d}) );
-        }
+        float yx = stb_perlin_noise3(x*period,0,(z+d)*period) * amplitude;
+        float yz = stb_perlin_noise3((x+d)*period,0,z*period) * amplitude;
+        normals.push_back( get_normal( {x,y,z}, {x+d,yx,z}, {x,yz,z+d}) );
     }
     std::vector<int> indices = triangulate(positions);
 
-    if (ntype == SURFACE_NORMALS)
-        normals = get_normals(positions, indices);
-
     return MeshDef{positions, indices, normals,
-        ntype, FRAGMENTED, 1, flags};
+        POSITION_NORMALS, FRAGMENTED, 1};
 }
 
-MeshDef MeshGen::Box(float w, float h, float d, MeshFlags flags)
+MeshDef MeshGen::Box(float w, float h, float d)
 {
     w /= 2, h /= 2, d /= 2;
     std::vector<vec3> positions{
@@ -54,23 +47,19 @@ MeshDef MeshGen::Box(float w, float h, float d, MeshFlags flags)
          unitX,  unitX,  unitZ,  unitZ,  unitY,  unitY};
 
     return MeshDef{positions, indices, normals,
-        SURFACE_NORMALS, FRAGMENTED, 1, flags};
+        SURFACE_NORMALS, FRAGMENTED, 1};
 }
 
-MeshDef MeshGen::Sphere(float radius, int n, MeshFlags flags)
+MeshDef MeshGen::Sphere(float radius, int n)
 {
-    NormalType ntype = flags & REMOVE_NORMALS ? NO_NORMALS : POSITION_NORMALS;
-    bool inverted = flags & INVERTED;
-
     auto p_mesh = par_shapes_create_subdivided_sphere(n);
     std::vector<vec3> positions, normals;
     for (int i = 0; i < p_mesh->npoints; i++) {
         vec3 point = radius * vec3{ p_mesh->points[3*i],
-                                    p_mesh->points[3*i+(inverted?2:1)],
-                                    p_mesh->points[3*i+(inverted?1:2)]};
+                                    p_mesh->points[3*i+1],
+                                    p_mesh->points[3*i+2]};
         positions.push_back(point);
-        if (ntype == POSITION_NORMALS)
-            normals.push_back( glm::normalize(point * (inverted?-1.f:1.f)) );
+        normals.push_back( glm::normalize(point) );
     }
 
     std::vector<int> indices;
@@ -79,7 +68,7 @@ MeshDef MeshGen::Sphere(float radius, int n, MeshFlags flags)
 
     par_shapes_free_mesh(p_mesh);
     return MeshDef{positions, indices, normals,
-        ntype, FRAGMENTED, .02f, flags};
+        POSITION_NORMALS, FRAGMENTED, .02f};
 }
 
 
