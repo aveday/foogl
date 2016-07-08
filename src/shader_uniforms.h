d3b0f14ebb2e17bc5a7a7440a79b6a7607aec96b
David Williams <david@aveday.net>
2016-07-09 00:19:18 +1000

    Move shader uniform locations to uniform_locations.h

---
+++ src/shader_uniforms.h
@@ -0,0 +1,9 @@
+#pragma once
+
+static const GLuint UNIFORM_MODEL = 1;
+
+static const GLuint UNIFORM_VIEW_MATRIX = 0;
+static const GLuint UNIFORM_VIEW_POSITION = 2;
+
+static const GLuint UNIFORM_TEXTURE = 3;
+static const GLuint UNIFORM_NORMALMAP = 4;
