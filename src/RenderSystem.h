0be99faf3ad0bb7cad5a2809492ea91ae97d08e9
David Williams <david@aveday.net>
2016-07-09 00:19:18 +1000

    Move shader uniform locations to uniform_locations.h

--- src/RenderSystem.h
+++ src/RenderSystem.h
@@ -4,12 +4,5 @@
 class RenderSystem {
 public:
     void run();
-
-private:
-    static const GLuint UNIFORM_MODEL = 1;
-    static const GLuint UNIFORM_TEXTURE = 2;
-
-    static const GLuint UNIFORM_VIEW_MATRIX = 0;
-    static const GLuint UNIFORM_VIEW_POSITION = 3;
 };
 
