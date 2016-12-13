2bf6b236d8ec038f955fcd7fed8524adccc798a0
David Williams <david@aveday.net>
2016-12-13 17:31:15 +1100

    Move shader_uniforms to RenderSystem.h

--- src/RenderSystem.h
+++ src/RenderSystem.h
@@ -4,5 +4,13 @@
 class RenderSystem {
 public:
     void run();
+private:
+    static const GLuint UNIFORM_MODEL = 1;
+
+    static const GLuint UNIFORM_VIEW_MATRIX = 0;
+    static const GLuint UNIFORM_VIEW_POSITION = 2;
+
+    static const GLuint UNIFORM_TEXTURE = 3;
+    static const GLuint UNIFORM_NORMALMAP = 4;
 };
 
